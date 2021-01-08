58b45c03f3831fc69d79be07f75ed75bc5901d35
degustafsson <debbi.gustafsson@yahoo.com>
2021-01-08 01:56:38 -0500

    1 file modified

--- index.js
+++ index.js
@@ -7,10 +7,12 @@ searchButton.addEventListener("click", () => {
 });
 
 //An asynchronous function to fetch data from the API.
-async function sendApiRequest() {
-  let response = await fetch(``);
-  console.log(response);
-}
+	let APP_ID = "47335b98"
+	let API_KEY = "102f477b4cfc27973b4b1ed6f278f1b9"
+  let response = await fetch(`https://api.edamam.com/search?app_id=${APP_ID}&app_key=${API_KEY}&q=pizza`);
+  console.log(response)
+  let data = await response.json()
+  console.log(data)
 
 //function that does something with the data received from the API. The name of the function should be customized to whatever you are doing with the data
 function useApiData(data) {}
